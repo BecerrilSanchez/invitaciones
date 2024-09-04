d84c842840b8f74715487df956615fcc874dcde3
BecerrilSanchez <marcosyerandy@gmail.com>
2024-09-03 21:02:52 -0600

    Se agrega API graphql para registro de invitaciones

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "proyectoaws": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
